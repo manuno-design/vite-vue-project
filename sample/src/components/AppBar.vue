77d9ed31001fe3b58e7c504f4113b244516b5f1d
manuno-design <kidnessfree@outlook.jp>
2023-01-28 19:15:19 +0000

    init commit

---
+++ sample/src/components/AppBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+defineProps<{ title: string, color: string }>()
+</script>
+
+<template>
+    <v-app-bar :color="color">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-title>{{ title }}</v-app-bar-title>
+    </v-app-bar>
+</template>
